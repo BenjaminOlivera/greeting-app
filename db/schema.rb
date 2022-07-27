23b0debb1dbf0d2c5cddb8e093612b8299ee9f0d
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-07-27 00:38:58 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_07_26_004303) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "greetings", force: :cascade do |t|
     t.string "name"
     t.text "message"
